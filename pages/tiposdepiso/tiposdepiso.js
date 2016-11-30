d0fbb984c12d1607fd195bb20c4788485e212a77
Adrian Ruiz <aerf2007@gmail.com>
2016-11-30 17:47:39 +0100

    + Mant Pisos: Edicion Inline [Proceso]

--- pages/tiposdepiso/tiposdepiso.js
+++ pages/tiposdepiso/tiposdepiso.js
@@ -1,3 +1,36 @@
+function guardar(boton){
+  var filaPiso =  boton.parentElement.parentElement;
+  boton.parentElement.parentElement.innerHTML = "<td class='col-md-1'>" +filaPiso.childNodes[0].innerHTML +"</td>"+
+  "<td class='col-md-3'>"+filaPiso.childNodes[1].childNodes[0].value+"</td>"+
+  "<td class='col-md-3'>"+filaPiso.childNodes[2].childNodes[0].value+"</td>"+
+  "<td class='col-md-3'><button class='btn btn-default' onclick='editar(this)' title='Editar'>📝</button>";
+  pisoEditando = [];
+}
+
+function editar(boton){
+    var filaPiso = boton.parentElement.parentElement;
+    console.log(filaPiso.cells[0]);
+    filaPiso.className += 'col-input';
+    filaPiso.innerHTML =
+    "<td class='col-md-1'>" +filaPiso.cells[0].innerHTML +"</td>"+
+    "<td class='col-md-3'><input class='form-control' value='"+filaPiso.cells[1].innerHTML+"' /></td>"+
+    "<td class='col-md-3'><input class='form-control' value='"+filaPiso.cells[2].innerHTML+"' /></td>"+
+    "<td class='col-md-3'><button class='btn btn-xs btn-default' title='Guardar' onclick='guardar(this)'>💾</button><button class='btn btn-xs btn-default' title='Cancelar' onclick='cancelar(this)'>⛔</button></td>";
+    pisoEditando = [filaPiso.childNodes[0].innerHTML,filaPiso.childNodes[1].innerHTML,filaPiso.childNodes[2].innerHTML];
+}
+
+function cancelar(boton){
+    var filaPiso = boton.parentElement.parentElement;
+    console.log(filaPiso.childNodes);
+    filaPiso.className += 'col-input';
+    filaPiso.innerHTML =
+    "<td class='col-md-1'>" +filaPiso.childNodes[1].innerHTML +"</td>"+
+    "<td class='col-md-3'><input class='form-control' value='"+filaPiso.childNodes[3].innerHTML+"' /></td>"+
+    "<td class='col-md-3'><input class='form-control' value='"+filaPiso.childNodes[5].innerHTML+"' /></td>"+
+    "<td class='col-md-3'><button class='btn btn-default' title='Guardar' onclick='guardar(this)'>💾</button><button class='btn btn-default' title='Cancelar' onclick='cancelar(this)'>⛔</button></td>";
+    pisoEditando = [filaPiso.childNodes[1].innerHTML,filaPiso.childNodes[3].innerHTML];
+}
+
 (function(){
   var datosPiso = [];
 
@@ -8,18 +41,4 @@
   //Piso en Edicion
   var pisoEditando = [];
 
-  for (var i = 0; i < botonesEditar.length; i++) {
-    botonesEditar[i].addEventListener("click",function(evt){
-      var filaPiso = document.getElementById("tiposdepiso").rows[evt.target.parentElement.parentElement.rowIndex];
-      document.getElementById("tiposdepiso").rows[evt.target.parentElement.parentElement.rowIndex].innerHTML = "<td class='col-md-1'>" +filaPiso.childNodes[1].innerHTML +"</td>"+
-      "<td class='col-md-3'><input class='form-control' value='"+filaPiso.childNodes[3].innerHTML+"' /></td>"+
-      "<td class='col-md-3'><input class='form-control' value='"+filaPiso.childNodes[5].innerHTML+"' /></td>"+
-      "<td class='col-md-3'><button class='btn btn-default' onclick='guardar(this)'>💾</button><button class='btn btn-default' onclick='cancelar(this)'>💾</button></td>";
-      pisoEditando = [filaPiso.childNodes[1].innerHTML,filaPiso.childNodes[3].innerHTML];
-
-    });
-  }
-  function guardar(boton){
-    console.log(boton);
-  }
 })();
