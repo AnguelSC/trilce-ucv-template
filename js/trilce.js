dce67647dbb46b0037c990e0077ee9e9b9ee37ce
DESKTOP-E5E4E3D\MIGUEL <design.anguel@gmail.com>
2016-11-30 20:05:03 -0500

    + Principal: Tree ya puede hacer click en small [Actualizacion]

--- js/trilce.js
+++ js/trilce.js
@@ -1,10 +1,11 @@
 var spanS = document.querySelectorAll(".tree span");
+console.info(spanS);
 for (var i = 0; i < spanS.length; i++) {
   if(spanS[i].nextSibling){
     spanS[i].nextSibling.nextSibling.style.display = 'none';
   }
   spanS[i].addEventListener('click',function (evt) {
-    var Elemento = evt.target.nextSibling;
+    var Elemento = ((evt.target.tagName == 'SMALL')?evt.target.parentElement.nextSibling:evt.target.nextSibling);
     if(Elemento){
       if(Elemento.nextSibling.style.display == 'none'){
           Elemento.nextSibling.style.display = 'block';
