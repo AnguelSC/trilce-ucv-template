a581d6d8a25b1bb23f539c5e6c596d27ab57a739
Adrian Ruiz <aerf2007@gmail.com>
2016-12-01 02:36:18 +0100

    +Mant Ambientes:Formulario[Proceso]

--- pages/mantambientes/mantambientes.js
+++ pages/mantambientes/mantambientes.js
@@ -1,16 +1,3 @@
-var spanS = document.querySelectorAll(".tree span");
-for (var i = 0; i < spanS.length; i++) {
-  if(spanS[i].nextSibling){
-    spanS[i].nextSibling.nextSibling.style.display = 'none';
-  }
-  spanS[i].addEventListener('click',function (evt) {
-    var Elemento = evt.target.nextSibling;
-    if(Elemento){
-      if(Elemento.nextSibling.style.display == 'none'){
-          Elemento.nextSibling.style.display = 'block';
-      }else{
-          Elemento.nextSibling.style.display = 'none';
-      }
-    }
-  });
+function agregarElemento(boton,nivelActual,nivelSiguente){
+  console.log(boton);
 }
